0e2ce6fee9ac00e5c33cd90d8ba0dd1e375919e3
Rashmi <rashmi.manandhar7@gmail.com>
2020-03-08 12:09:00 -0400

    feat: get tweet list for dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
+import Dashboard from "./dashboard";
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    return <div>Starter Code</div>;
+    return <div>{this.props.loading === true ? null : <Dashboard />}</div>;
   }
+}
+
+function mapStateToProps({ authUser }) {
+  return {
+    loading: authUser === null
+  };
 }
 
 export default connect()(App);
